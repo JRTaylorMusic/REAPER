2f1e448bad9bbd4f481b2116cc8ce52c4826c09e
JR Taylor <81386894+JRTaylorMusic@users.noreply.github.com>
2021-09-22 13:30:27 -0500

    Create JRT_Extend selection to start of session.lua

---
+++ Arrange Editing/JRT_Extend selection to start of session.lua
@@ -0,0 +1,19 @@
+reaper.Undo_BeginBlock()
+reaper.PreventUIRefresh(1)
+local pos = reaper.GetCursorPosition()
+
+in_point, out_point = reaper.GetSet_LoopTimeRange( false, false, 0, 0, false )
+
+start = 0.0
+
+if in_point == out_point
+  then 
+  reaper.GetSet_LoopTimeRange(1, 0, start, pos, 0)  
+  else
+  reaper.GetSet_LoopTimeRange(1, 0, start, out_point, 0)
+end
+
+reaper.Main_OnCommand(40718, 0)
+
+reaper.PreventUIRefresh(-1)
+reaper.Undo_EndBlock("JRT_Extend selection to start of session", -1)
