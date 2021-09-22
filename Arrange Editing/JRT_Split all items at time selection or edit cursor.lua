13e277e424698c2892cce309ee146c19a64432f2
JR Taylor <81386894+JRTaylorMusic@users.noreply.github.com>
2021-09-22 13:30:23 -0500

    Update JRT_Split all items at time selection or edit cursor.lua

--- Arrange Editing/JRT_Split all items at time selection or edit cursor.lua
+++ Arrange Editing/JRT_Split all items at time selection or edit cursor.lua
@@ -4,48 +4,37 @@
 -- @provides [main]
 
 -- SAVE INITIAL SELECTED ITEMS
-init_sel_items = {}
-function SaveSelectedItems (table)
-  for i = 0, reaper.CountSelectedMediaItems(0)-1 do
-    table[i+1] = reaper.GetSelectedMediaItem(0, i)
-  end
-end -- END INITIAL SELECTED ITEMS
+    init_sel_items = {}
+    function SaveSelectedItems (sel_items)
+      for i = 0, reaper.CountSelectedMediaItems(0)-1 do
+        sel_items[i+1] = reaper.GetSelectedMediaItem(0, i)
+      end
+    end
 
 -- RESTORE INITIAL SELECTED ITEMS
-function RestoreSelectedItems (table)
-  reaper.Main_OnCommand(40289, 0) -- Unselect all items
-  for _, item in ipairs(table) do
-    reaper.SetMediaItemSelected(item, true)
-  end
-end -- END RESTORE INITIAL SELECTED ITEMS
+    function RestoreSelectedItems (sel_items)
+      reaper.Main_OnCommand(40289, 0) -- Unselect all items
+      for _, item in ipairs(sel_items) do
+        reaper.SetMediaItemSelected(item, true)
+      end
+    end
   
-in_point, out_point = reaper.GetSet_LoopTimeRange( false, false, 0, 0, false )
-
---[[function UnselectAllItems()
-  for  i = 0, reaper.CountMediaItems(0) do
-    reaper.SetMediaItemSelected(reaper.GetMediaItem(0, i), false)
-  end
-end--]]--
+-- GET IN/OUT POINTS
+    in_point, out_point = reaper.GetSet_LoopTimeRange( false, false, 0, 0, false )
 
 -- Main function
-function main()
-  if in_point == out_point
-    then 
-    reaper.SelectAllMediaItems(0, true)
-    reaper.Main_OnCommand(40012, 0) --split at edit or play cursor
-    RestoreSelectedItems(init_sel_items)
-    else
-    reaper.SelectAllMediaItems(0, true)
-    reaper.Main_OnCommand(40061, 0) --split at time selection
-    RestoreSelectedItems(init_sel_items)
-     
-   
-  end
-  
-  
-  
-
-end
+    function main()
+      if in_point == out_point
+        then 
+        reaper.SelectAllMediaItems(0, true)
+        reaper.Main_OnCommand(40012, 0) --split at edit or play cursor
+        RestoreSelectedItems(init_sel_items)
+        else
+        reaper.SelectAllMediaItems(0, true)
+        reaper.Main_OnCommand(40061, 0) --split at time selection
+        RestoreSelectedItems(init_sel_items)
+      end
+    end
 
 reaper.PreventUIRefresh(1)
 
